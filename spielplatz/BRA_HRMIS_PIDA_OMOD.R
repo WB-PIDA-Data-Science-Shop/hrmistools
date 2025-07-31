a39f272bc9c5de97f9d6a4cd8e5426633e40b19d
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2025-07-31 16:28:52 -0400

    updates
    updates

--- spielplatz/BRA_HRMIS_PIDA_OMOD.R
+++ spielplatz/BRA_HRMIS_PIDA_OMOD.R
@@ -2,7 +2,7 @@
 ######## TESTING THE INTENDED HARMONIZATION PROCESS WITH THE BRAZIL DATA #######
 ################################################################################
 
-paklist <- c("tidyverse", "polyglotr")
+paklist <- c("tidyverse", "polyglotr", "readxl", "purrr", "furrr", "writexl")
 
 ## silently load the libraries
 invisible(lapply(paklist,
@@ -12,22 +12,27 @@ invisible(lapply(paklist,
 
                    }))
 
-#### lets put together the organization module of the Brazil data
-fileloc <- "//egvpi/egvpi/data/harmonization/HRM/BRA/data-raw/6. Wage Bill AL/3. Microdados"
+plan(multisession, workers = 6)
+set.seed(1789)
 
+# read-in data ------------------------------------------------------------
+file_path <- "//egvpi/egvpi/data/harmonization/HRM/BRA/data-raw/6. Wage Bill AL/3. Microdados"
 
-### read in the data
-active_contract_tbl <-
-  list.files(path = fileloc,
+active_alagoastbl_list <-
+  list.files(path = file_path,
              pattern = "^Ativos_[0-9]{4}\\.xlsx$",
              full.names = T) |>
-  map(readxl::read_xlsx)
+  future_map(
+    \(file) read_xlsx(file, na = c("", "-"))
+  )
 
-inactive_contract_tbl <-
-  list.files(path = fileloc,
+inactive_alagoastbl_list <-
+  list.files(path = file_path,
              pattern = "^Inativos_[0-9]{4}\\.xlsx$",
              full.names = T) |>
-  map(readxl::read_xlsx)
+  future_map(
+    \(file) read_xlsx(file, na = c("", "-"))
+  )
 
 
 ### lets quickly label the raw data
@@ -87,79 +92,98 @@ inact_list <- c(
 ####### lets combine the list of datasets into an active and inactive dataframe
 
 #### quickly harmonize column classes
-active_contract_tbl <- harmonize_col_class(active_contract_tbl)
-inactive_contract_tbl <- harmonize_col_class(inactive_contract_tbl)
+active_alagoastbl_list <- harmonize_col_class(active_alagoastbl_list)
+inactive_alagoastbl_list <- harmonize_col_class(inactive_alagoastbl_list)
 
-active_contract_tbl <- bind_rows(active_contract_tbl)
-inactive_contract_tbl <- bind_rows(inactive_contract_tbl)
+active_alagoas_tbl <- bind_rows(active_alagoastbl_list)
+inactive_alagoas_tbl <- bind_rows(inactive_alagoastbl_list)
 
 
 #### ready to prepare the organization module
 
-
-### for active
-active_org_tbl <-
-  active_contract_tbl |>
-  mutate(org_id = paste0(ORGAO, " - ", COD_ORGAO)) |>
-  mutate(org_name_native = ORGAO) |>
-  mutate(country_code = "BRA") |>
-  mutate(country_name = "Brazil") |>
-  mutate(adm1_name = "Alagoas") |>
-  mutate(adm1_code = "AL") |>
-  mutate(org_parent = NA) |>
-  mutate(org_child = NA) |>
-  dplyr::select(org_id, org_name_native,
-                country_code, country_name,
-                starts_with("adm", ignore.case = FALSE),
-                org_parent,
-                org_child) |>
+##### a few diagnostics
+check_dt <-
+active_alagoas_tbl |>
+  dplyr::select(ANO_PAGAMENTO, ORGAO, COD_ORGAO) |>
   unique()
 
-### including organizational name translations
+check_list <-
+active_alagoas_tbl |>
+  group_by(ANO_PAGAMENTO) |>
+  group_split() |>
+  map(~ .x |>
+        group_by(ORGAO, COD_ORGAO) |>
+        summarize(n = n(), .groups = "drop") |>
+        pivot_wider(names_from = COD_ORGAO, values_from = n))
 
-active_org_tbl <-
-  active_org_tbl |>
-  merge(tibble(org_name_native = unique(active_org_tbl$org_name_native),
-               org_name_en = vectorize_gt(vector = unique(active_org_tbl$org_name_native),
-                                          source_language = "pt")),
-        by = "org_name_native",
-        all.x = TRUE) |>
-  as_tibble()
+names(check_list) <- unique(active_alagoas_tbl$ANO_PAGAMENTO)
+
+write_xlsx(check_list, "spielplatz/orgao_check.xlsx")
 
 
 
-### for the inactive
-inactive_org_tbl <-
-  inactive_contract_tbl |>
-  mutate(org_id = paste0(ORGAO, " - ", COD_ORGAO)) |>
-  mutate(org_name_native = ORGAO) |>
-  mutate(country_code = "BRA") |>
-  mutate(country_name = "Brazil") |>
-  mutate(adm1_name = "Alagoas") |>
-  mutate(adm1_code = "AL") |>
-  mutate(org_parent = NA) |>
-  mutate(org_child = NA) |>
-  mutate(org_date = as.Date(paste(ANO_PAGAMENTO,
-                                  sprintf("%2d", MES_REFERENCIA),
-                                  "01",
-                                  sep = "-"))) |>
-  dplyr::select(org_id, org_name_native,
-                country_code, country_name,
-                starts_with("adm", ignore.case = FALSE),
-                org_parent,
-                org_child, org_date) |>
-  unique()
-
+# ### for active
+# active_alagoas_tbl <-
+#   active_alagoas_tbl |>
+#   mutate(org_id = paste0(ORGAO, " - ", COD_ORGAO)) |>
+#   mutate(org_name_native = ORGAO) |>
+#   mutate(country_code = "BRA") |>
+#   mutate(country_name = "Brazil") |>
+#   mutate(adm1_name = "Alagoas") |>
+#   mutate(adm1_code = "AL") |>
+#   mutate(org_parent = NA) |>
+#   mutate(org_child = NA) |>
+#   dplyr::select(org_id, org_name_native,
+#                 country_code, country_name,
+#                 starts_with("adm", ignore.case = FALSE),
+#                 org_parent,
+#                 org_child) |>
+#   unique()
+#
 # ### including organizational name translations
 #
-# inactive_org_tbl <-
-#   inactive_org_tbl |>
-#   merge(tibble(org_name_native = unique(inactive_org_tbl$org_name_native),
-#                org_name_en = vectorize_gt(vector = unique(inactive_org_tbl$org_name_native),
+# active_org_tbl <-
+#   active_org_tbl |>
+#   merge(tibble(org_name_native = unique(active_org_tbl$org_name_native),
+#                org_name_en = vectorize_gt(vector = unique(active_org_tbl$org_name_native),
 #                                           source_language = "pt")),
 #         by = "org_name_native",
-#         all.x = TRUE)
-
+#         all.x = TRUE) |>
+#   as_tibble()
+#
+#
+#
+# ### for the inactive
+# inactive_org_tbl <-
+#   inactive_contract_tbl |>
+#   mutate(org_id = paste0(ORGAO, " - ", COD_ORGAO)) |>
+#   mutate(org_name_native = ORGAO) |>
+#   mutate(country_code = "BRA") |>
+#   mutate(country_name = "Brazil") |>
+#   mutate(adm1_name = "Alagoas") |>
+#   mutate(adm1_code = "AL") |>
+#   mutate(org_parent = NA) |>
+#   mutate(org_child = NA) |>
+#   mutate(org_date = as.Date(paste(ANO_PAGAMENTO,
+#                                   sprintf("%2d", MES_REFERENCIA),
+#                                   "01",
+#                                   sep = "-"))) |>
+#   dplyr::select(org_id, org_name_native,
+#                 country_code, country_name,
+#                 starts_with("adm", ignore.case = FALSE),
+#                 org_parent,
+#                 org_child, org_date) |>
+#   unique()
+#
+# # ### including organizational name translations
+# #
+# # inactive_org_tbl <-
+# #   inactive_org_tbl |>
+# #   merge(tibble(org_name_native = unique(inactive_org_tbl$org_name_native),
+# #                org_name_en = vectorize_gt(vector = unique(inactive_org_tbl$org_name_native),
+# #                                           source_language = "pt")),
+# #         by = "org_name_native",
+# #         all.x = TRUE)
 
 
 
