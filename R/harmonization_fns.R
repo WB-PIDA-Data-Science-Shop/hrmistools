50197fcbc90a4d7d70c6a5e94561414cde8d73cc
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2025-07-31 08:37:26 -0400

    updates

--- R/harmonization_fns.R
+++ R/harmonization_fns.R
@@ -41,7 +41,38 @@ vectorize_gt <- function(vector,
 
 }
 
+#' A function to identify inconsistent column names across data frames
+#'
+#' This function checks for consistency in column names across a list of data frames.
+#' It returns the symmetric difference between the shared columns (those present in all data frames)
+#' and the union of all column names (those present in any data frame).
+#'
+#' @param data_list A list of data frames to check.
+#'
+#' @return A character vector of column names that are not shared across all data frames.
+#' These are the inconsistent or unique column names that appear in only some of the data frames.
+#'
+#' @examples
+#' df1 <- data.frame(a = 1, b = 2)
+#' df2 <- data.frame(a = 3, c = 4)
+#' df_list <- list(df1, df2)
+#' find_inconsistent_colnames(df_list)
+#' # Returns: "b" "c"
+#'
+#' @importFrom purrr map reduce
+#' @importFrom tibble tibble
+#' @export
+find_inconsistent_colnames <- function(data_list) {
+  shared_cols <- data_list |>
+    map(\(data) tibble(colnames = colnames(data))) |>
+    reduce(intersect)
 
+  all_cols <- data_list |>
+    map(\(data) tibble(colnames = colnames(data))) |>
+    reduce(union)
+
+  symdiff(shared_cols, all_cols)
+}
 
 
 
