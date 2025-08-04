16191166cd9d07cdd12391c36000fdcd26f9436e
Galileu Kim <galileukim@worldbank.org>
2025-08-04 17:29:15 -0400

    polish the worker module

--- spielplatz/bra_hrmis_worker.R
+++ spielplatz/bra_hrmis_worker.R
@@ -14,8 +14,8 @@ set.seed(1789)
 
 # read-in data ------------------------------------------------------------
 file_path <- "//egvpi/egvpi/data/harmonization/HRM/BRA/data-raw/6. Wage Bill AL/3. Microdados"
-
-plan(multisession, workers = 3)
+#
+# plan(multisession, workers = 3)
 
 workers_active_list <-
   list.files(
@@ -23,7 +23,7 @@ workers_active_list <-
     pattern = "^Ativos_[0-9]{4}\\.xlsx$",
     full.names = T
   ) |>
-  future_map(
+  map(
     \(file) read_xlsx(
       file, na = c("", "-"), col_types = "text"
     ) |>
@@ -36,14 +36,14 @@ workers_inactive_list <-
     pattern = "^Inativos_[0-9]{4}\\.xlsx$",
     full.names = T
   ) |>
-  future_map(
+  map(
     \(file) read_xlsx(
       file, na = c("", "-"), col_types = "text"
     ) |>
       clean_names()
   )
-
-future::plan(sequential)
+#
+# future::plan(sequential)
 
 # harmonization -----------------------------------------------------------
 # what we want is two things: (1) uniqueness in the cross-section (entities are uniquely identified) and
@@ -54,19 +54,13 @@ inactive_inconsistent_cols <- workers_inactive_list |>
   pull(colnames)
 
 # Worker
-#   - Worker ID (worker_id)
-#   - Organization ID (org_id)
-#   - Timestamp (org_date)
+#   - Worker ID (contract_id)
 #   - Date of Birth (birth_date)
 #   - Gender (gender)
 #   - Education Attainment (educat7)
 #   - Tribe (tribe)
 #   - Race (race)
 #   - Status (active/retired)
-#   - Country Code, (country_code)
-#   - Country Name, (country_name)
-#   - Administration 1 Name, (adm1_name)
-#   - Administration 1 Code, (adm1_code)
 
 # identify if a dataset contains inconsistent colnames
 workers_inactive_list |>
@@ -79,7 +73,7 @@ dictionary_worker <- tibble(
     "ano_pagamento", "orgao", "mes_referencia", "matricula", "cpf", "data_nascimento", "genero", "escolaridade"
   ),
   to = c(
-    "year", "department", "month", "worker_id", "national_id", "date_birth", "gender", "educat7"
+    "year", "department", "month", "contract_id", "worker_id", "date_birth", "gender", "educat7"
   )
 )
 
@@ -150,50 +144,128 @@ workers_active <- workers_active |>
 # check for uniqueness per year
 worker_quality_check <- create_agent(tbl = workers_active) |>
   rows_distinct(
-    columns = c(year, worker_id),
-    step_id = "check_unique_worker_id",
+    columns = contract_id,
+    segments = vars(year),
+    step_id = "check_unique_contract_id",
     label = "Check for uniqueness of worker ID per year"
   ) |>
   rows_distinct(
-    columns = c(worker_id),
-    preconditions = . %>% distinct(worker_id, national_id),
-    step_id = "check_unique_worker_and_national_id",
+    columns = c(contract_id),
+    preconditions = . %>% distinct(contract_id, worker_id),
+    step_id = "check_unique_worker_and_worker_id",
     label = "Check for uniqueness of worker ID and national ID combinations"
   )
 
 worker_quality_check |>
   interrogate()
 
+# deduplicate -------------------------------------------------------------
+# matricula is the contract ID
 workers_id <- workers_active |>
   distinct(
-    worker_id, national_id
+    contract_id,
+    worker_id
   )
 
-# matricula is the worker ID
-
-national_id_duplicate <- workers_id |>
-  group_by(national_id) |>
-  summarise(
-    n = n_distinct(worker_id),
-    .groups = "drop"
-  ) |>
-  filter(n > 1)
-
-workers_id |>
+worker_id_duplicate <- workers_id |>
   group_by(worker_id) |>
   summarise(
-    n = n_distinct(national_id),
+    n = n_distinct(contract_id),
     .groups = "drop"
   ) |>
   filter(n > 1)
 
-national_id_duplicate |>
+workers_active |>
   inner_join(
-    workers_active
+    worker_id_duplicate,
+    by = c("worker_id")
   ) |>
-  arrange(
-    national_id
+  arrange(worker_id) |>
+  select(
+    worker_id,
+    contract_id,
+    year,
+    date_birth,
+    gender
+  )
+
+# generalize into functions
+find_duplicate_ids <- function(data, identifier){
+  data |>
+    add_count(
+      {{identifier}}
+    ) |>
+    filter(n > 1)
+}
+
+contract_id_duplicate_national <- workers_id |>
+  group_by(contract_id) |>
+  summarise(
+    n_worker_id = n_distinct(worker_id),
+    correct_worker_id = first(worker_id),
+    .groups = "drop"
   )
 
 workers_active |>
-  filter(worker_id == "12946")
+  inner_join(
+    contract_id_duplicate_national,
+    by = c("contract_id")
+  ) |>
+  arrange(contract_id) |>
+  select(
+    worker_id,
+    contract_id,
+    year,
+    date_birth,
+    gender
+  ) |>
+  distinct(worker_id)
+
+# decision: override national id with the first national id for each contract_id
+# 98 national ids affected
+workers_active <- workers_active |>
+  left_join(
+    contract_id_duplicate_national,
+    by = "contract_id"
+  ) |>
+  mutate(
+    worker_id = if_else(
+      n_worker_id > 1,
+      correct_worker_id,
+      worker_id
+    )
+  ) |>
+  ungroup() |>
+  select(
+    -n_worker_id,
+    -correct_worker_id
+  )
+
+# extract worker module
+# Worker
+#   - Worker ID (contract_id)
+#   - Date of Birth (birth_date)
+#   - Gender (gender)
+#   - Education Attainment (educat7)
+#   - Tribe (tribe)
+#   - Race (race)
+#   - Status (active/retired)
+workers_module <- workers_active |>
+  distinct(
+    worker_id,
+    ref_date,
+    date_birth,
+    gender,
+    educat7,
+    status = "active"
+  )
+
+# we need to have a set protocol for how to disambiguate between
+# conflicting attributes of the same worker_id
+# if there are multiple educat7 values, we take the lowest one
+workers_module |> 
+  add_count(worker_id, ref_date) |> 
+  filter(n > 1)
+
+workers_module |> 
+  filter(worker_id == "15676013672")
