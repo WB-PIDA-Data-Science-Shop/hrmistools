5625b2ea0714d3d62421a5ff618135ba5096a4e0
Galileu Kim <galileukim@worldbank.org>
2025-10-15 11:13:18 -0400

    switch from org_date to ref_date

---
+++ spielplatz/bra_hrmis_movement.R
@@ -0,0 +1,41 @@
+# set-up ------------------------------------------------------------------
+library(readr)
+library(dplyr)
+library(here)
+
+# read-in data ------------------------------------------------------------
+contract <- read_rds(
+  here("spielplatz", "bra_hrmis_contract.rds")
+)
+
+worker <- read_rds(
+  here("inst/extdata/bra_hrmis_worker.rds")
+)
+
+# infer movement ----------------------------------------------------------
+# the movement table should have:
+# 1) contract_id
+# 2) ref_date
+# 3) event type: hire, dismissal, retirement, reallocation.
+
+# first infer hire
+# a hire is defined as a new contract when the worker
+# was not present in the dataset in the previous period
+contract_hire <- contract |>
+  rename(
+    ref_date = org_date
+  ) |>
+  group_by(contract_id) |>
+  summarise(
+    first_ref_date = first(ref_date)
+  )
+
+# if the worker does not appear in the previous ref_date, this is a hire
+contract_hire |>
+  mutate(
+    ref_date_lag = ref_date - years(1)
+  ) |>
+  anti_join(
+    worker |> select(worker_id, ref_date),
+    by = c("worker_id", "ref_date_lag" = "ref_date")
+  )
