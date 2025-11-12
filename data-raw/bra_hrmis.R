22fac32bd03a317a7c4c707a16488c6d214e915e
Galileu Kim <galileukim@worldbank.org>
2025-11-12 18:07:25 -0500

    generate demo data
    create a demo of the Brazil HRMIS data, hashing out the CPF #24

---
+++ data-raw/bra_hrmis.R
@@ -0,0 +1,59 @@
+library(dplyr)
+library(readxl)
+library(furrr)
+library(writexl)
+library(digest)
+library(here)
+
+devtools::load_all()
+
+dir.create(
+  here("inst", "extdata"),
+  recursive = TRUE
+)
+
+# read-in data ------------------------------------------------------------
+file_path <- "//egvpi/egvpi/data/harmonization/HRM/BRA/data-raw/6. Wage Bill AL/3. Microdados"
+
+plan(multisession, workers = 6)
+
+contract_active_list <-
+  list.files(path = file_path,
+             pattern = "^Ativos_[0-9]{4}\\.xlsx$",
+             full.names = T) |>
+  future_map(\(file) read_xlsx(file, na = c("", "-"), col_types = "text"))
+
+contract_inactive_list <-
+  list.files(path = file_path,
+             pattern = "^Inativos_[0-9]{4}\\.xlsx$",
+             full.names = T) |>
+  future_map(\(file) read_xlsx(file, na = c("", "-"), col_types = "text"))
+
+# combine data ------------------------------------------------------------
+contract_active <- contract_active_list |>
+  bind_rows() |>
+  filter(
+    MES_REFERENCIA == 9 &
+      ANO_PAGAMENTO >= 2014
+  )
+
+contract_inactive <- contract_inactive_list |>
+  bind_rows() |>
+  filter(
+    MES_REFERENCIA == 9 &
+      ANO_PAGAMENTO >= 2014
+  )
+
+bra_hrmis <- contract_active |>
+  bind_rows(
+    contract_inactive
+  )
+
+# anonymize the CPF
+bra_hrmis <- bra_hrmis |>
+  mutate(
+    CPF = map_chr(CPF, digest, algo = "sha256")
+  )
+
+# write-out ---------------------------------------------------------------
+usethis::use_data(bra_hrmis, overwrite = TRUE)
