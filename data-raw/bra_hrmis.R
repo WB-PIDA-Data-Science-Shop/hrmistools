fb4f1d90da0dd36fd7f6ac58a397f62cdf35ee38
Galileu Kim <galileukim@worldbank.org>
2025-11-13 18:04:21 -0500

    produce clean data
    and populate the `analytics` article #26

--- data-raw/bra_hrmis.R
+++ data-raw/bra_hrmis.R
@@ -56,10 +56,27 @@ bra_hrmis <- bra_hrmis |>
     CPF = map_chr(CPF, digest, algo = "sha256")
   )
 
-# contract module ---------------------------------------------------------\
+# import clean data -------------------------------------------------------
+bra_hrmis_contract <- read_rds(
+  here("spielplatz", "data", "bra_hrmis_contract.rds")
+) |>
+  filter(year >= 2014)
 
-# worker module
+bra_hrmis_worker <- read_rds(
+  here("spielplatz", "data", "bra_hrmis_worker.rds")
+) |>
+  filter(
+    lubridate::year(ref_date) >= 2014
+  )
 
+bra_hrmis_org <- read_rds(
+  here("spielplatz", "data", "bra_hrmis_organization.rds")
+)
 
 # write-out ---------------------------------------------------------------
 usethis::use_data(bra_hrmis, overwrite = TRUE)
+
+# modules
+usethis::use_data(bra_hrmis_contract, overwrite = TRUE)
+usethis::use_data(bra_hrmis_worker, overwrite = TRUE)
+usethis::use_data(bra_hrmis_org, overwrite = TRUE)
